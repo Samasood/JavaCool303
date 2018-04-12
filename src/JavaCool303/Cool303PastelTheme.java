aa98808ee49e9cb0d5a7372e20d637f6a90beaef
parthkhanna150 <parth.khanna@mail.mcgill.ca>
2018-04-12 17:19:27 -0400

    Pastel and Winter

--- src/JavaCool303/Cool303PastelTheme.java
+++ src/JavaCool303/Cool303PastelTheme.java
@@ -1,5 +1,25 @@
 package JavaCool303;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
 public class Cool303PastelTheme extends Cool303Theme{
-    
+        public Cool303PastelTheme() {
+        buttonColor = Color.GREEN;
+        buttonTextFont = new Font("", Font.BOLD, 6);
+        buttonDimension = new Dimension(10, 5);
+        //buttonBorder = new Border()
+
+        containerColor = Color.DARK_GRAY;
+        containerTextFont = new Font("", Font.BOLD, 8);
+        containerDimension = new Dimension(200, 100);
+        //buttonBorder = new Border()
+
+        rootColor = Color.YELLOW;
+        rootTextFont = new Font("", Font.BOLD, 10);
+        rootDimension = new Dimension(300, 200);
+        //buttonBorder = new Border()
+    }
+
 }
--- src/JavaCool303/Cool303WinterTheme.java
+++ src/JavaCool303/Cool303WinterTheme.java
@@ -1,5 +1,25 @@
 package JavaCool303;
 
-public class Cool303WinterTheme extends Cool303Theme{
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+public class Cool303WinterTheme extends Cool303Theme {
+
+    public Cool303WinterTheme() {
+        buttonColor = Color.BLUE;
+        buttonTextFont = new Font("", Font.ITALIC, 6);
+        buttonDimension = new Dimension(10, 5);
+        //buttonBorder = new Border()
+
+        containerColor = Color.GRAY;
+        containerTextFont = new Font("", Font.ITALIC, 8);
+        containerDimension = new Dimension(200, 100);
+        //buttonBorder = new Border()
+
+        rootColor = Color.CYAN;
+        rootTextFont = new Font("", Font.ITALIC, 10);
+        rootDimension = new Dimension(300, 200);
+        //buttonBorder = new Border()
+    }
 }
